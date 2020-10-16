3e6b6152137d293d5593d062f5c615a7475dffa3
Jason Paul <jasonepaul@outlook.com>
2020-10-16 17:09:08 -0600

    Initial commit

---
+++ weather/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+# Create your views here.
+
+
+def homepage(request):
+
+    x = [1, 2, 3, 4, 5]
+    y = [1, 2, 3, 4, 5]
+
+    plot = figure(title='Line Graph', plot_width=600, plot_height=400)
+
+    plot.line(x, y, line_width=2)
+
+    script, div = components(plot)
+    print("test views.homepage()")
+
+    return render(request, "weather/base.html", {'script': script, 'div': div})
