fd45fdd658ec98ed11835044b1a6bd967cb6bbd3
Jason Paul <jasonepaul@outlook.com>
2020-10-29 15:37:16 -0600

    Rename app

--- weather_app/wsgi.py
+++ weather_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for yyc_weather project.
+WSGI config for weather_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yyc_weather.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_app.settings')
 
 application = get_wsgi_application()
