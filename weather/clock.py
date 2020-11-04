4293cbb9ebfbfcf894ebb3ca26cf779c335e9af7
Jason Paul <jasonepaul@outlook.com>
2020-11-04 12:36:52 -0700

    Add django.setup() to clock.py

--- weather/clock.py
+++ weather/clock.py
@@ -1,11 +1,15 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from weather.model_manager import set_stats, update_weather_tables
+import django
+
+
+django.setup()
 
 # for background tasts
 schedule = BlockingScheduler()
 
 
-schedule.add_job(set_stats, 'cron', year=2020, month=11, day=4, hour=19, timezone='UTC')
+schedule.add_job(set_stats, 'cron', year=2020, month=11, day=4, hour=23, timezone='UTC')
 schedule.add_job(update_weather_tables, 'cron', hour=10, timezone='UTC')
 
 schedule.start()
