2cf9ec349715752311e64a59857d46250dff1b18
Jason Paul <jasonepaul@outlook.com>
2020-11-04 14:54:36 -0700

    Add *args, **options to handle(); change to BackgroundScheduler

--- weather/management/commands/scheduler.py
+++ weather/management/commands/scheduler.py
@@ -1,10 +1,10 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from weather.model_manager import set_stats, update_weather_tables
 from django.core.management.base import BaseCommand
 
 
 # for background tasts
-schedule = BlockingScheduler()
+schedule = BackgroundScheduler()
 
 
 schedule.add_job(set_stats, 'cron', year=2020, month=11, day=4, hour=22, timezone='UTC')
@@ -13,10 +13,10 @@ schedule.add_job(update_weather_tables, 'cron', hour=10, timezone='UTC')
 
 @schedule.scheduled_job('interval', minutes=1)
 def timed_job():
-    print('This job is run every three minutes.')
+    print('This job is run every minute.')
 
 
 class Command(BaseCommand):
 
-    def handle(self):
+    def handle(self, *args, **options):
         schedule.start()
